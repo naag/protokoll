ed2b01cc4c95a1ca5501b0e478e40d39e55b894f
Peter Bücker <peter.buecker@pressrelations.de>
2015-01-16 12:19:57 +0100

    Default configuration logs to STDOUT and enables the gem

--- lib/protokoll/railtie.rb
+++ lib/protokoll/railtie.rb
@@ -4,8 +4,11 @@ require 'action_controller/log_subscriber'
 
 module Protokoll
   class Railtie < Rails::Railtie
+    config.logger = Logger.new(STDOUT)
     config.protokoll = ActiveSupport::OrderedOptions.new
-    config.protokoll.enabled = false
+    config.protokoll.enabled = true
+    config.protokoll.client = :client_ip
+    config.protokoll.request_id = :request_id
 
     initializer :protokoll do |app|
       Protokoll.setup(app) if app.config.protokoll.enabled
